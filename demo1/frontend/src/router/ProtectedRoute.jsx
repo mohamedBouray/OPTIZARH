1933fd2eff09fc08ad9e47601a88317f59f13be9
Sybous Mohamed <mohamed.sybous@gmail.com>
2026-04-20 00:18:40 +0100

    Delete demo1 directory

--- demo1/frontend/src/router/ProtectedRoute.jsx
+++
@@ -1,6 +0,0 @@
-import { Navigate, Outlet } from "react-router-dom";
-
-export default function ProtectedRoute() {
-    const isAuthenticated = document.cookie.includes('XSRF-TOKEN'); 
-    return isAuthenticated ? <Outlet /> : <Navigate to="/login" />;
-}
